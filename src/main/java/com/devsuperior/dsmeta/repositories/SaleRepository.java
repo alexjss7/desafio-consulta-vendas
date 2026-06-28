eb49a640fce2303b4feab2815603c7898ec0c4dd
Alex Silva <alexjulio0209@gmail.com>
2026-06-28 16:14:06 -0300

    Implemented sales report and sales summary.

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,37 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleReportDTO(obj.id, obj.date, obj.amount, obj.seller.name) "
+            + "FROM Sale obj "
+            + "WHERE obj.date BETWEEN :startDate AND :endDate "
+            + "AND (:name IS NULL OR LOWER(obj.seller.name) LIKE LOWER(CONCAT('%', :name, '%')))")
+    Page<SaleReportDTO> report(
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("name") String name,
+            Pageable pageable);
+
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) "
+            + "FROM Sale obj "
+            + "WHERE obj.date BETWEEN :startDate AND :endDate "
+            + "GROUP BY obj.seller.name "
+            + "ORDER BY SUM(obj.amount) DESC ")
+    List<SaleSummaryDTO> summary(
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,14 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -12,12 +18,41 @@ import com.devsuperior.dsmeta.repositories.SaleRepository;
 @Service
 public class SaleService {
 
-	@Autowired
-	private SaleRepository repository;
-	
-	public SaleMinDTO findById(Long id) {
-		Optional<Sale> result = repository.findById(id);
-		Sale entity = result.get();
-		return new SaleMinDTO(entity);
-	}
+    @Autowired
+    private SaleRepository repository;
+
+    public SaleMinDTO findById(Long id) {
+        Optional<Sale> result = repository.findById(id);
+        Sale entity = result.get();
+        return new SaleMinDTO(entity);
+    }
+
+    public Page<SaleReportDTO> report(String startDateStr, String endDateStr, String name, Pageable pageable) {
+
+        LocalDate endDate = (endDateStr == null || endDateStr.isEmpty())
+                ? LocalDate.now()
+                : LocalDate.parse(endDateStr);
+
+        LocalDate startDate = (startDateStr == null || startDateStr.isEmpty())
+                ? endDate.minusYears(1)
+                : LocalDate.parse(startDateStr);
+
+        String sellerName = (name == null || name.isEmpty())
+                ? null
+                : name;
+        return repository.report(startDate, endDate, sellerName, pageable);
+    }
+
+    public List<SaleSummaryDTO> summary(String startDateStr, String endDateStr) {
+
+        LocalDate endDate = (endDateStr == null || endDateStr.isEmpty())
+                ? LocalDate.now()
+                : LocalDate.parse(endDateStr);
+
+        LocalDate startDate = (startDateStr == null || startDateStr.isEmpty())
+                ? endDate.minusYears(1)
+                : LocalDate.parse(startDateStr);
+
+        return repository.summary(startDate, endDate);
+    }
 }
